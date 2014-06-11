06f31762a91c771a1dcd02dac7083246e66b135c
Aaron Bieber <deftly@gmail.com>
2014-06-11 16:38:07 -0600

    yum

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -1,14 +1,10 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins/"))
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
 (file-exists-p "/usr/local/go/misc/emacs/go-mode-load.el")
 	       (add-to-list 'auto-mode-alist '("go$" . go-mode))
-
-;(tool-bar-mode -1)
-(menu-bar-mode -1)
-(column-number-mode +1)
-(show-paren-mode +1)
 
 (require 'package)
 (add-to-list 'package-archives 
@@ -18,12 +14,12 @@
 	       "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
-(require 'nlinum)
-
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (column-number-mode +1)
 (show-paren-mode +1)
+
+(require 'nlinum)
 
 ;(if (daemonp)
 ;    (add-hook 'after-make-frame-functions
@@ -31,9 +27,12 @@
 ;                (load-theme 'tronesque t)))
 ;    (load-theme 'tronesque t))
 
+
 ;(load-theme 'zenburn 1)
-(load-theme 'tango-dark 1)
+;(load-theme 'tango-dark 1)
 ;(load-theme 'tronesque t)
+;(load-theme 'solarized-dark t)
+(load-theme 'flatland t)
 
 (evil-mode 1)
 

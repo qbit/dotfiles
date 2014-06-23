88be8f78c0c9c14f98c56711d3f600a3c0d42ae7
Aaron Bieber <deftly@gmail.com>
2014-06-22 19:24:29 -0600

    c-tab ftw

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -35,7 +35,7 @@
 ;(evil-mode 1)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
-(global-set-key "\M- " 'hippie-expand)
+(global-set-key (kbd "<C-tab>") 'hippie-expand)
 
 (global-git-gutter-mode +1)
 (custom-set-variables

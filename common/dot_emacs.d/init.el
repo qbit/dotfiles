61a9fc3ac77abe31c9344a331c885cd578a87e09
Aaron Bieber <deftly@gmail.com>
2014-06-24 18:31:54 -0600

    orgmode hook

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -38,6 +38,10 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (global-set-key (kbd "<C-tab>") 'hippie-expand)
 
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
 
 (global-git-gutter-mode +1)
 (custom-set-variables

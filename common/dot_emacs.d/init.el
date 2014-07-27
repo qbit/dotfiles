2a14eaa3933658a3b5037a0b9ab058297d66c34d
Aaron Bieber <deftly@gmail.com>
2014-07-26 20:55:07 -0600

    move mail stuff to its own file

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -7,52 +7,14 @@
 (if (file-exists-p "/usr/local/go/misc/emacs/go-mode-load.el")
     (add-to-list 'auto-mode-alist '("go$" . go-mode)))
 
-(setq
- user-mail-address "deftly@gmail.com"
- user-full-name "Aaron Bieber")
-
-(when (file-exists-p "/usr/local/share/emacs/site-lisp/mu4e/mu4e.el")
-  (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e/")
-  (require 'mu4e)
-  (require 'org-mu4e)
-  (require 'smtpmail)
-
-  (when (file-directory-p "/data/user_storage/qbit/Maildir")
-    (setq mu4e-maildir (expand-file-name "/data/user_storage/qbit/Maildir")))
-
-  (setq mu4e-compose-signature "")
-  (setq mu4e-drafts-folder "/[Gmail].Drafts")
-  (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
-  (setq mu4e-trash-folder  "/[Gmail].Trash")
-
-  ;; don't save message to Sent Messages, GMail/IMAP will take care of this
-  (setq mu4e-sent-messages-behavior 'delete)
-
-  ;; setup some handy shortcuts
-  (setq mu4e-maildir-shortcuts
-	'(("/INBOX"             . ?i)
-	  ("/[Gmail].Sent Mail" . ?s)
-	  ("/[Gmail].Trash"     . ?t)))
-  
-  (setq mu4e-headers-skip-duplicates t)
-  (setq mu4e-html2text-command "html2text -b 72")
-
-  (setq send-mail-function 'smtpmail-send-it
-	starttls-use-gnutls t
-	smtpmail-starttls-credentials
-	'(("smtp.gmail.com" 587 nil nil))
-	smtpmail-auth-credentials
-	(expand-file-name "~/.authinfo.gpg")
-	smtpmail-default-smtp-server "smtp.gmail.com"
-	smtpmail-smtp-server "smtp.gmail.com"
-	smtpmail-smtp-service 587
-	smtpmail-debug-info t))
-
-
 (let ((default-directory user-emacs-directory))
       (normal-top-level-add-subdirs-to-load-path))
 
 (defvar site-lisp-directory (concat user-emacs-directory "site-lisp/"))
+
+(when (file-exists-p "/usr/local/share/emacs/site-lisp/mu4e/mu4e.el")
+  (add-to-list 'load-path "/usr/localshare/emacs/site-lisp/mu4e/")
+  (require 'mail))
 
 (file-exists-p "~quicklisp/slime-helper.el")
 	       (load (expand-file-name "~/quicklisp/slime-helper.el")

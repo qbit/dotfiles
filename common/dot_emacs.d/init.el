6ca12355d653e1e3b29de3e928a4e7cdf301f03c
Aaron Bieber <deftly@gmail.com>
2014-10-03 16:06:11 -0600

    fix slime loading and fix package archives

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -32,9 +32,9 @@
   (add-to-list 'load-path "/usr/localshare/emacs/site-lisp/mu4e/")
   (require 'mail))
 
-;(file-exists-p "~quicklisp/slime-helper.el")
-;	       (load (expand-file-name "~/quicklisp/slime-helper.el")
-;	       (setq inferior-lisp-program "sbcl"))
+(file-exists-p "~quicklisp/slime-helper.el")
+	       (load (expand-file-name "~/quicklisp/slime-helper.el")
+	       (setq inferior-lisp-program "sbcl"))
 
 (guru-global-mode +1)
 

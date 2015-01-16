76fa3e33478a2ac4cb82533c91fa2f18d25c5607
Aaron Bieber <deftly@gmail.com>
2015-01-15 19:59:26 -0700

    nosave

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -95,6 +95,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(git-gutter:added-sign "++")
  '(git-gutter:deleted-sign "--")
  '(git-gutter:modified-sign "  ")

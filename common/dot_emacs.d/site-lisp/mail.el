179a3b93888d47aacdc7a805cd643d3993c77df3
Aaron Bieber <aaron@bolddaemon.com>
2015-06-19 09:26:17 -0600

    mu4e, I can't quit you!

--- common/dot_emacs.d/site-lisp/mail.el
+++ common/dot_emacs.d/site-lisp/mail.el
@@ -2,4 +2,17 @@
  user-mail-address "aaron@bolddaemon.com"
  user-full-name "Aaron Bieber")
 
+(require 'mu4e)
+(setq mu4e-maildir "~/Mail")
+(setq mu4e-get-mail-command "mbsync -q fastmail")
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-stream-type 'starttls
+      smtpmail-default-smtp-server "mail.messagingengine.com"
+      smtpmail-smtp-server "mail.messagingengine.com"
+      smtpmail-smtp-service 587)
+
+;; don't keep message buffers around
+(setq message-kill-buffer-on-exit t)
+
 (provide 'mail)

fbe263c3ac8a59f02ca9ee1a01cbc0d89c4de42b
Aaron Bieber <aaron@bolddaemon.com>
2015-09-04 13:55:21 -0600

    fix indenting and some type definitions

--- slip/dot_xmonad/xmonad.hs
+++ slip/dot_xmonad/xmonad.hs
@@ -9,87 +9,75 @@ import XMonad.Layout.Gaps
 import XMonad.Util.EZConfig
 import XMonad.Util.Run(spawnPipe)
 import System.IO
+import Data.Monoid
 
 import qualified XMonad.StackSet as W
 
+main :: IO()
 main = do
         status <- spawnPipe myXmoStatus
         xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
                    {
-                   normalBorderColor = "#666666"
+                     normalBorderColor = "#666666"
                    , focusedBorderColor = "darkgrey"
-                   , terminal = "urxvtc"
+                   , terminal = "uxterm"
                    , workspaces = myWorkspaces
                    , layoutHook = myLayoutHook
                    , logHook = myLogHook status
                    , manageHook = manageDocks <+> myManageHook
-                                    <+> manageHook defaultConfig
+                                  <+> manageHook defaultConfig
                    }
                    `removeKeysP` ["M-p"] -- don't clober emacs.
                    `additionalKeysP` myKeys
 
---myLogHook h = dynamicLogWithPP $ myDzenPP { ppOutput = hPutStrLn h }
+myLogHook :: Handle -> X ()
 myLogHook h = dynamicLogWithPP $ myXmoPP {ppOutput = hPutStrLn h}
 
-myKeys = [
-  ("M-r", spawn "dmenu_run")
-  , ("M-z", spawn "xmonad --recompile && xmonad --restart")
-  ]
+myKeys :: [([Char], X ())]
+myKeys =
+    [
+     ("M-r", spawn "dmenu_run")
+    , ("M-z", spawn "xmonad --recompile && xmonad --restart")
+    , ("M-i", spawn "~/.screenlayout/internal.sh")
+    , ("M-e", spawn "~/.screenlayout/external.sh")
+    ]
 
+xmobarEscape :: [Char] -> [Char]
 xmobarEscape = concatMap doubleLts
-  where doubleLts '<' = "<<"
-        doubleLts x   = [x]
+    where doubleLts '<' = "<<"
+          doubleLts x   = [x]
 
-myWorkspaces            :: [String]
-myWorkspaces            = clickable . (map xmobarEscape) $ ["emacs","browser","irc","4","5","6","7","8","console"]
---    xmobar compat
+myWorkspaces :: [String]
+myWorkspaces = clickable . (map xmobarEscape) $ ["emacs","browser","irc","4","5","6","7","8","console"]
     where clickable l = [ "<action=xdotool key alt+" ++ show (n) ++ ">" ++ ws ++ "</action>" |
                           (i,ws) <- zip [1..9] l,
                           let n = i ]
---    Dzen compat
---    where clickable l     = [ "^ca(1,xdotool key alt+" ++ show (n) ++ ")" ++ ws ++ "^ca()" |
---                              (i,ws) <- zip [1..] l,
---                              let n = i ]
---myLayoutHook = avoidStruts $ smartBorders ( tiled ||| ptiled ||| mtiled ||| pmtiled ||| full )
-myLayoutHook = avoidStruts $ smartBorders ( tiled ||| ptiled |||  full )
-  where
-    full     = named "X" $ Full
-    --mtiled   = named "M" $ spacing 3 $ Mirror tiled
-    --pmtiled  = named "pM" $ spacing 3 $ Mirror ptiled
-    tiled    = named "T" $ spacing 3 $ Tall 1 (5/100) (2/(1+(toRational(sqrt(5)::Double))))
-    ptiled   = named "pT" $ spacing 3 $ gaps [(U,60), (L,60), (R,60), (D,60)] $ Tall 1 (5/100) (2/(1+(toRational(sqrt(5)::Double))))
-    --tiled  = spacing 3 $ named "T" $ Tall 1 (3/100) (1/2)
 
+myLayoutHook = avoidStruts $ smartBorders ( tiled ||| ptiled |||  full )
+    where
+      full     = named "X" $ Full
+      tiled    = named "T" $ spacing 3 $ Tall 1 (5/100) (2/(1+(toRational(sqrt(5)::Double))))
+      ptiled   = named "pT" $ spacing 3 $ gaps [(U,60), (L,60), (R,60), (D,60)] $ Tall 1 (5/100) (2/(1+(toRational(sqrt(5)::Double))))
+
+myManageHook :: Query (Data.Monoid.Endo WindowSet)
 myManageHook = composeAll
-               [ className =? "MPlayer"        --> doFloat
+               [
+                className =? "MPlayer"        --> doFloat
                , className =? "XCalc"          --> doFloat
                , className =? "Chrome"         --> doF (W.shift (myWorkspaces !! 1)) -- send to ws 2
                , className =? "XConsole"        --> doF (W.shift (myWorkspaces !! 8))
                ]
 
+myXmoStatus :: String
 myXmoStatus = "~/.cabal/bin/xmobar"
 
+myXmoPP :: PP
 myXmoPP = xmobarPP
-          { ppCurrent = xmobarColor "#443740" "" . wrap " " " "
+          {
+            ppCurrent = xmobarColor "#443740" "" . wrap " " " "
           , ppHidden  = xmobarColor "#ffffff" "" . wrap " " " "
-          , ppHiddenNoWindows = \x -> "" -- xmobarColor "#777777" "" . wrap " " " "
           , ppUrgent  = xmobarColor "#ff0000" "" . wrap " " " "
           , ppSep     = "     "
           , ppLayout  = xmobarColor "#ffffff" "" . wrap "|" "|"
           , ppTitle   = xmobarColor "#ffffff" "" . shorten 25
           }
-
---myDzenStatus = "dzen2 -ta 'l'" ++ myDzenStyle
---myDzenStyle  = " -h '20' -fg '#777777' -bg '#222222'"
-
---myDzenPP  = dzenPP
---            { ppCurrent = dzenColor "#3399ff" "" . wrap " " " "
---            , ppHidden  = dzenColor "#dddddd" "" . wrap " " " "
---            , ppHiddenNoWindows = dzenColor "#777777" "" . wrap " " " "
---            , ppUrgent  = dzenColor "#ff0000" "" . wrap " " " "
---            , ppSep     = "     "
---            , ppLayout  = dzenColor "#aaaaaa" "" . wrap "^ca(1,xdotool key alt+space)· " " ·^ca()"
---            , ppTitle   = dzenColor "#ffffff" ""
---                          . wrap "^ca(1,xdotool key alt+k)^ca(2,xdotool key alt+shift+c)"
---                                "                          ^ca()^ca()" . shorten 20 . dzenEscape
---            }

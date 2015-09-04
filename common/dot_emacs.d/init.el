92a7b9728f90feb8032afd76d89eb759ebcb9dd6
Aaron Bieber <aaron@bolddaemon.com>
2015-09-04 14:11:10 -0600

    haskell stuff

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -140,6 +140,23 @@ With negative prefix, apply to -N lines above."
     ((haskell-process-use-ghci . t)
      (haskell-indent-spaces . 4)))))
 
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(custom-set-variables
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t))
+
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
 
 (set-face-attribute 'default t :height 100)
 

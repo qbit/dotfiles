f5de1e13ba4daa711eabadc2bb88b354bdfea360
Aaron Bieber <aaron@bolddaemon.com>
2016-01-18 19:35:57 -0700

    helm ftw

--- common/dot_emacs.d/site-lisp/keycuts.el
+++ common/dot_emacs.d/site-lisp/keycuts.el
@@ -1,7 +1,10 @@
 (global-set-key (kbd "<backtab>") 'hippie-expand)
 (global-set-key "\M-g" 'magit-status)
 
-(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-x")                          'undefined)
+(global-set-key (kbd "M-x")                          'helm-M-x)
+(global-set-key (kbd "C-x r b")                      'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f")                      'helm-find-files)
 
 (require 'rect-mark)
 ; add some smarts to the normal kill / paste stuff

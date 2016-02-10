51fa509afdf7683daeb085353264d0efd8ffc590
Aaron Bieber <aaron@bolddaemon.com>
2016-02-10 10:13:32 -0700

    use eww for rich mails, show fancy chars and exclude sig

--- common/dot_emacs.d/site-lisp/mail.el
+++ common/dot_emacs.d/site-lisp/mail.el
@@ -3,9 +3,14 @@
  user-full-name "Aaron Bieber")
 
 (require 'mu4e)
+(require 'mu4e-contrib)
+
+(setq mu4e-html2text-command 'mu4e-shr2text)
 (setq mu4e-maildir "~/Mail")
 (setq mu4e-get-mail-command "mbsync -q fastmail")
 (setq mu4e-headers-include-related t)
+(setq mu4e-use-fancy-chars t)
+(setq mu4e-compose-signature-auto-include nil)
 
 (setq message-send-mail-function 'smtpmail-send-it
       smtpmail-stream-type 'starttls

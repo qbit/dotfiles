989d9b565eb24eb021cda88d0c828e3f0eabdbf3
Aaron Bieber <aaron@bolddaemon.com>
2016-08-04 07:38:04 -0600

    back to auto-moe

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -86,8 +86,8 @@
 ;;(load-theme 'solarized-dark t)
 ;;(load-theme 'flatland t)
 (require 'moe-theme)
-(load-theme 'moe-dark t)
-;;(require 'moe-theme-switcher)
+;;(load-theme 'moe-dark t)
+(require 'moe-theme-switcher)
 ;;(powerline-moe-theme)
 ;;(load-theme 'eink-light t)
 

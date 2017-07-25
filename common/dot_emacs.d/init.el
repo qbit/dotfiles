282a14b8ad76ffe10fb3424efbb3de78410a1041
Aaron Bieber <aaron@bolddaemon.com>
2017-07-25 10:36:31 -0600

    purge haskell, change scratch msg

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -1,9 +1,10 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+
+(setq initial-scratch-message ";; ╔═╗┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬\n;; ╚═╗│  ├┬┘├─┤ │ │  ├─┤\n;; ╚═╝└─┘┴└─┴ ┴ ┴ └─┘┴ ┴\n\n")
 
 (setenv "PATH"
 	(concat
@@ -189,20 +190,6 @@ With negative prefix, apply to -N lines above."
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 (global-git-gutter-mode +1)
-
-
-(require 'haskell-interactive-mode)
-(require 'haskell-process)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-
-(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-;;(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
 
 (set-face-attribute 'default t :height 100)
 

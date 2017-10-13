2207fd4c9de8f10b35e27a8dda11e7bb7c4be566
Aaron Bieber <aaron@bolddaemon.com>
2017-10-13 08:03:10 -0600

    sync

--- common/dot_emacs.d/site-lisp/custom.el
+++ common/dot_emacs.d/site-lisp/custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(org-agenda-files
    (quote
-    (slime web-mode spaceline scpaste scad-mode org-plus-contrib nyan-mode neotree magit golint go-eldoc git-gutter flycheck counsel base16-theme all-the-icons))))
+    ("~/new_org/gtd.org" "~/new_org/journal_2017-10.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

26050c293cbbb2c838a51ef48c807161eaee6bbc
Aaron Bieber <aaron@bolddaemon.com>
2017-10-13 07:01:37 -0600

    log time and get todos / journal going

--- common/dot_emacs.d/site-lisp/orgie.el
+++ common/dot_emacs.d/site-lisp/orgie.el
@@ -8,11 +8,15 @@
 (setq org-agenda-files (file-expand-wildcards "~/new_org/*.org"))
 (setq org-journal-dir "~/new_org/journal/")
 (setq org-mobile-directory "~/new_org/MobileOrg")
+(setq org-log-done 'time)
 
 (setq org-capture-templates
-            `(("j" "Journal" entry (file+datetree ,(format-time-string "~/new_org/journal_%Y-%m.org"))
-	       "* %?\n")))
-
+      `(("t" "Todo"
+	 entry (file+headline "~/new_org/gtd.org" "Tasks")
+	 "* TODO %?\n  %i\n  %a")
+        ("j" "Journal"
+	 entry (file+olp+datetree "~/new_org/journal-%Y-%m.org")
+	 "* %?\nEntered on %U\n  %i\n  %a")))
 
 (provide 'orgie)
 

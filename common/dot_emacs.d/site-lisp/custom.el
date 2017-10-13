d2f5d08c67d61c24599ded7d27aa58c1c3ab9073
Aaron Bieber <aaron@bolddaemon.com>
2017-10-13 07:01:59 -0600

    sync

--- common/dot_emacs.d/site-lisp/custom.el
+++ common/dot_emacs.d/site-lisp/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (nyan-mode web-mode spaceline scpaste scad-mode org-plus-contrib neotree magit golint go-eldoc git-gutter flycheck counsel base16-theme all-the-icons))))
+    (slime web-mode spaceline scpaste scad-mode org-plus-contrib nyan-mode neotree magit golint go-eldoc git-gutter flycheck counsel base16-theme all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

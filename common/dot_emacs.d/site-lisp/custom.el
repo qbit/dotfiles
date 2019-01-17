e6157b813135a99079dc5c77d90d9f742af77174
Aaron Bieber <aaron@bolddaemon.com>
2019-01-17 07:29:00 -0700

    sync

--- common/dot_emacs.d/site-lisp/custom.el
+++ common/dot_emacs.d/site-lisp/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (web-mode spaceline slime scpaste scad-mode rust-mode org-plus-contrib nord-theme neotree forge magit golint go-eldoc git-gutter flycheck counsel base16-theme all-the-icons))))
+    (fzf web-mode spaceline slime scpaste scad-mode rust-mode rubocopfmt rubocop org-plus-contrib nord-theme neotree golint go-eldoc git-gutter forge flycheck fill-column-indicator counsel base16-theme all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

5b781c21a351256a6a7ca9256e389f3d895aee5b
Aaron Bieber <aaron@bolddaemon.com>
2019-01-20 09:44:15 -0700

    use org vs "new_org" - fix journal file name.. start prepending

--- common/dot_emacs.d/site-lisp/orgie.el
+++ common/dot_emacs.d/site-lisp/orgie.el
@@ -4,19 +4,19 @@
 
 (require 'org)
 
-(setq org-directory "~/new_org")
-(setq org-agenda-files (file-expand-wildcards "~/new_org/*.org"))
-(setq org-journal-dir "~/new_org/journal/")
-(setq org-mobile-directory "~/new_org/MobileOrg")
+(setq org-directory "~/org")
+(setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
+(setq org-journal-dir "~/org/journal/")
+(setq org-mobile-directory "~/org/mobile")
 (setq org-log-done 'time)
 
 (setq org-capture-templates
       `(("t" "Todo"
-	 entry (file+headline "~/new_org/gtd.org" "TODOs")
-	 "* TODO %?\n  :PROPERTIES:\n  :LOGGING: TODO(!) WAIT(!) DONE(!) CANCELED(!)\n  :END:\n")
+	 entry (file+headline "~/org/gtd.org" "TODOs")
+	 "* TODO %?\n  :PROPERTIES:\n  :LOGGING: TODO(!) WAIT(!) DONE(!) CANCELED(!)\n  :END:\n" :prepend t)
         ("j" "Journal"
-	 entry (file+olp+datetree "~/new_org/journal-%Y-%m.org")
-	 "* %?\nEntered on %U\n  %i\n  %a")))
+	 entry (file+olp+datetree "~/org/journal.org")
+	 "* %?\nEntered on %U\n  %i\n  %a" :prepend t)))
 
 (provide 'orgie)
 

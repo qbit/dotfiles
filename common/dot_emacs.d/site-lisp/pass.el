8818b30115bf44cb3408702cbee94a391eacfb7d
Aaron Bieber <aaron@bolddaemon.com>
2019-01-25 17:13:33 -0700

    enable email via mu4e, add password-store capability

---
+++ common/dot_emacs.d/site-lisp/pass.el
@@ -0,0 +1,9 @@
+;;; pass --- pass config for emacs
+;;; Commentary:
+;;; Code:
+
+(add-to-list 'load-path "/usr/local/share/examples/password-store/emacs/")
+
+(require 'password-store)
+
+(provide 'pass)

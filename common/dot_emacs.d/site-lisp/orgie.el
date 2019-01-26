264a506a995c6239485b751b5c06a2d720087e5a
Aaron Bieber <aaron@bolddaemon.com>
2019-01-25 17:11:58 -0700

    enable habit, make todo capture a bit more clean

--- common/dot_emacs.d/site-lisp/orgie.el
+++ common/dot_emacs.d/site-lisp/orgie.el
@@ -3,6 +3,7 @@
 ;;; Code:
 
 (require 'org)
+(require 'org-habit)
 
 (setq org-directory "~/org")
 (setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
@@ -12,8 +13,17 @@
 
 (setq org-capture-templates
       `(("t" "Todo"
-	 entry (file+headline "~/org/gtd.org" "TODOs")
-	 "* TODO %?\n  :PROPERTIES:\n  :LOGGING: TODO(!) WAIT(!) DONE(!) CANCELED(!)\n  :END:\n" :prepend t)
+	 entry (file+headline "~/org/todo.org" "TODOs")
+	 ,(concat
+	  "* TODO %?\n"
+	  "SCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n"
+	  ":PROPERTIES:\n"
+	  ":FILE: %a \n"
+	  ":LOGGING: TODO(!) WAIT(!) DONE(!) CANCELED(!)\n"
+	  ":END:\n") :prepend t)
+	("b" "Bug"
+	 entry (file+olp+datetree "~/org/bugs.org" "Bugs")
+	 "* BUG %?\nEntered on %U\n  :PROPERTIES:\n  :FILE: %a\n  :END:\n" :prepend t)
         ("j" "Journal"
 	 entry (file+olp+datetree "~/org/journal.org")
 	 "* %?\nEntered on %U\n  %i\n  %a" :prepend t)))

b7c021a567d122ff966bbe29c5730f264b009d7a
Aaron Bieber <aaron@bolddaemon.com>
2019-01-25 17:12:40 -0700

    re-order things a bit - enable battery-mode

--- common/dot_emacs.d/site-lisp/ui.el
+++ common/dot_emacs.d/site-lisp/ui.el
@@ -2,32 +2,35 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (set-default-font "Go Regular:pixelsize=15")
+(require 'spaceline-config)
+
 (setq inhibit-startup-screen t)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (column-number-mode +1)
 (show-paren-mode +1)
 (global-font-lock-mode 1)
+(display-battery-mode 1)
 
-(setq initial-scratch-message (concat
-			       (shell-command-to-string "fortune -o | sed -e 's/^/;; /g'")
-			       "\n\n"))
+(setq initial-scratch-message
+      (concat
+       (shell-command-to-string "fortune -o | sed -e 's/^/;; /g'")
+       "\n\n"))
 
 (setq whitespace-style '(trailing lines space-before-tab)
       whitespace-line-column 80)
 
-(set-face-attribute 'default t :height 100)
-
-;;(load-theme 'base16-default-light 1)
-(load-theme 'nord 1)
-(require 'spaceline-config)
 (spaceline-emacs-theme)
 
 (ivy-mode 1)
 
 (setq x-selection-timeout 10)
+
+;;(load-theme 'base16-default-dark 1)
+(load-theme 'nord 1)
+;;(load-theme 'moe-dark 1)
 
 (provide 'ui)
 

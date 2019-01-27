c8b05049ec81d5bc647c10ee1897d95bab3da98e
Aaron Bieber <aaron@bolddaemon.com>
2019-01-27 09:30:03 -0700

    more organizing, use moe-theme for now

--- common/dot_emacs.d/site-lisp/ui.el
+++ common/dot_emacs.d/site-lisp/ui.el
@@ -2,9 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'spaceline-config)
 
 (setq inhibit-startup-screen t)
+(setq initial-scratch-message
+      (concat
+       (shell-command-to-string "fortune -o | sed -e 's/^/;; /g'")
+       "\n\n"))
+(setq whitespace-style '(trailing lines space-before-tab)
+      whitespace-line-column 80)
+(setq x-selection-timeout 10)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -13,26 +19,21 @@
 (show-paren-mode +1)
 (global-font-lock-mode 1)
 (display-battery-mode 1)
-
+(ivy-mode 1)
 (global-git-gutter-mode +1)
 
-(setq initial-scratch-message
-      (concat
-       (shell-command-to-string "fortune -o | sed -e 's/^/;; /g'")
-       "\n\n"))
+;;; Nord theme
+;;; - currently there is a bug that prevents loading as daemon:
+;;;   https://github.com/arcticicestudio/nord-emacs/issues/59
+;;(require 'spaceline-config)
+;;(spaceline-emacs-theme)
+;; (load-theme 'nord 1)
 
-(setq whitespace-style '(trailing lines space-before-tab)
-      whitespace-line-column 80)
+;; (load-theme 'base16-default-dark 1)
 
-(spaceline-emacs-theme)
-
-(ivy-mode 1)
-
-(setq x-selection-timeout 10)
-
-;;(load-theme 'base16-default-dark 1)
-(load-theme 'nord 1)
-;;(load-theme 'moe-dark 1)
+(require 'moe-theme)
+(require 'powerline)
+(require 'moe-theme-switcher) ;; Gives us dark at night, light during the day
 
 (provide 'ui)
 

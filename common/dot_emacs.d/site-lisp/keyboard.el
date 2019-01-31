275e0e712bb58f9db39262bd944abc4b6412be1f
Aaron Bieber <aaron@bolddaemon.com>
2019-01-30 19:37:28 -0700

    binding for fzf

--- common/dot_emacs.d/site-lisp/keyboard.el
+++ common/dot_emacs.d/site-lisp/keyboard.el
@@ -11,7 +11,7 @@
 (global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-;;(global-set-key (kbd "C-x C-F") 'fzf)
+(global-set-key (kbd "C-c z") 'counsel-fzf)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
 (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
 (global-set-key (kbd "<f1> l") 'counsel-find-library)

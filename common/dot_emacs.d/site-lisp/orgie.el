0c1eea23718dd756a4ef663431ea5137f399e888
Aaron Bieber <aaron@bolddaemon.com>
2019-01-31 17:47:24 -0700

    use org-protocol and add some capture templates for it

--- common/dot_emacs.d/site-lisp/orgie.el
+++ common/dot_emacs.d/site-lisp/orgie.el
@@ -4,6 +4,7 @@
 
 (require 'org)
 (require 'org-habit)
+(require 'org-protocol)
 
 (setq org-directory "~/org")
 (setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
@@ -19,7 +20,15 @@
 (setq org-log-done 'time)
 
 (setq org-capture-templates
-      `(("t" "Todo"
+      `(("t" "TODO"
+	 entry (file+headline "~/org/todo.org" "TODOs")
+	 ,(concat
+	  "* TODO %?\n"
+	  "SCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n"
+	  ":PROPERTIES:\n"
+	  ":LOGGING: TODO(!) WAIT(!) DONE(!) CANCELED(!)\n"
+	  ":END:\n") :prepend t)
+	("f" "TODO with File"
 	 entry (file+headline "~/org/todo.org" "TODOs")
 	 ,(concat
 	  "* TODO %?\n"
@@ -31,6 +40,11 @@
 	("b" "Bug"
 	 entry (file+olp+datetree "~/org/bugs.org" "Bugs")
 	 "* BUG %?\nEntered on %U\n  :PROPERTIES:\n  :FILE: %a\n  :END:\n" :prepend t)
+	("p" "Protocol"
+	 entry (file+headline "~/org/links.org" "Links")
+         "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?")
+	("L" "Protocol Link" entry (file+headline "~/org/links.org" "Links")
+         "* %? %:link\n%:description\n")
         ("j" "Journal"
 	 entry (file+olp+datetree "~/org/journal.org")
 	 "* %?\nEntered on %U\n  %i\n  %a" :prepend t)))

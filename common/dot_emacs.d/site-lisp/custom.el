ce7d0eb56c30979e5ff6825c2ba5821cef952291
Aaron Bieber <aaron@bolddaemon.com>
2019-02-08 12:22:37 -0700

    sync

--- common/dot_emacs.d/site-lisp/custom.el
+++ common/dot_emacs.d/site-lisp/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (web-mode slime scpaste scad-mode rust-mode rubocop org-plus-contrib neotree golint go-eldoc git-gutter fzf forge flycheck counsel company-lsp base16-theme arduino-mode))))
+    (yaml-mode weechat web-mode swiper smex slime scpaste scad-mode rust-mode rubocop org-plus-contrib ido-completing-read+ golint go-eldoc git-gutter forge flycheck company-lsp base16-theme arduino-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

e10acd2ad1aa56f7d14ba45a5975da01fde313a7
Aaron Bieber <aaron@bolddaemon.com>
2019-03-01 08:30:16 -0700

    sync

--- common/dot_emacs.d/site-lisp/custom.el
+++ common/dot_emacs.d/site-lisp/custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(org-agenda-files
    (quote
-    (yaml-mode weechat web-mode smex slime scpaste scad-mode rust-mode rubocop org-plus-contrib ido-completing-read+ golint go-eldoc git-gutter forge flycheck company-lsp base16-theme arduino-mode))))
+    ("~/org/bios.org" "~/org/budget.org" "~/org/bugs.org" "~/org/health.org" "~/org/houses.org" "~/org/inbox.org" "~/org/init.org" "~/org/journal.org" "~/org/keyboards.org" "~/org/links.org" "~/org/openbsd_edgerouter_rtc.org" "~/org/ports.org" "~/org/projects.org" "~/org/todo.org" "~/org/work.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

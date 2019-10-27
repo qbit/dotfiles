1897a7527ba56463682368b73e348dfbb5515bbe
Aaron Bieber <aaron@bolddaemon.com>
2019-10-27 10:40:27 -0600

    sync

--- common/dot_emacs.d/site-lisp/custom.el
+++ common/dot_emacs.d/site-lisp/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (smex scpaste org-plus-contrib go-mode git-gutter ido-completing-read+ forge flycheck base16-theme))))
+    (smex scpaste org-plus-contrib ido-completing-read+ go-mode git-gutter forge flycheck base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

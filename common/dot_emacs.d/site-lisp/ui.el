d6da365da9825c943463102940dde177f043da0c
Aaron Bieber <aaron@bolddaemon.com>
2020-01-02 08:31:26 -0700

    nova theme

--- common/dot_emacs.d/site-lisp/ui.el
+++ common/dot_emacs.d/site-lisp/ui.el
@@ -33,9 +33,10 @@
 (display-time-mode 1)
 (global-git-gutter-mode +1)
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'base16-nord 1)
+;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'base16-nova 1)
 ;;(load-theme 'eink 1)
+;;(load-theme 'berrys-theme 1)
 
 (provide 'ui)
 

46754700243f171d4b6ee04c40a6e129da81728e
Aaron Bieber <aaron@bolddaemon.com>
2020-01-02 08:27:52 -0700

    use use-package

--- common/dot_emacs.d/init.el
+++ common/dot_emacs.d/init.el
@@ -17,18 +17,43 @@
 	    (normal-top-level-add-subdirs-to-load-path)))
 	 load-path)))
 
+(if (file-directory-p "/usr/local/share/emacs/site-lisp/mu4e")
+    (require 'email))
+(if (file-exists-p "~/.emacs.d/diary")
+    (diary))
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(use-package base16-theme)
+;;(use-package berrys-theme)
+;;(use-package nord-theme)
+;;(use-package moe-theme)
+
+(use-package flycheck)
+(use-package forge)
+(use-package git-gutter)
+(use-package go-mode)
+(use-package ido-completing-read+)
+(use-package lsp-mode)
+(use-package magit)
+(use-package org)
+(use-package scpaste)
+(use-package smex)
+
 (require 'notifications)
-(require 'functions)
 (require 'tls)
-(require 'pkgs)
 (require 'config)
 (require 'hooks)
 (require 'keyboard)
 (require 'orgie)
 (if (file-directory-p "/usr/local/share/emacs/site-lisp/mu4e")
-    (require 'email))
+      (require 'email))
 (require 'ui)
 
-(if (file-exists-p "~/.emacs.d/diary")
-    (diary))
-(eshell)
+
+(load "server")
+(unless (server-running-p) (server-start))

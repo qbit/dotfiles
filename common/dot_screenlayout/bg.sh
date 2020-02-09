6de4f130bc6bc2d7ffb31130baac440e5d388a0c
Aaron Bieber <aaron@bolddaemon.com>
2020-02-09 15:36:39 -0700

    light mode

--- common/dot_screenlayout/bg.sh
+++ common/dot_screenlayout/bg.sh
@@ -6,4 +6,4 @@
 #xbattbar -i green -o "olive drab" -I blue -O red &
 
 pkill lemonbar-xft
-while true; do ~/bin/bar; sleep 5; done | lemonbar-xft -d -f "Fantasque Sans Mono:pixelsize=14" -B "#2E3440"  -F "#D8DEE9" | sh &
+while true; do ~/bin/bar; sleep 5; done | lemonbar-xft -d -f "Fantasque Sans Mono:pixelsize=14" -B "#FFFFEA"  -F "#000000" | sh &

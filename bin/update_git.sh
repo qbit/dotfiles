92bae78c5cb548785b1c2abf084b8ba033e4f115
Aaron Bieber <aaron@bolddaemon.com>
2020-06-02 06:39:53 -0600

    move to a script for pulling openbsd sources.

---
+++ bin/update_git.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Update a git repo that has a "upstream" remote
+
+msg() {
+	echo "==> ${1}"
+}
+
+msg "Updating ${PWD}"
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if git remote | grep -q upstream; then
+	msg "Found upstream"
+	if [ "${BRANCH}" = "master" ]; then
+		msg "Pulling upstream -> master"
+		git pull upstream master
+	else
+		msg "Fetching all"
+		git fetch --all
+		msg "Don't forget to merge or pull!"
+	fi
+else
+	msg "Origin is upstream"
+	if [ "${BRANCH}" = "master" ]; then
+		msg "Pulling from origin:master"
+		git pull
+	else
+		msg "Fetching all"
+		git fetch --all
+		msg "Don't forget to merge or pull!"
+	fi
+fi

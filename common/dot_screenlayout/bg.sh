93f8a0f1483e8c25c190f2ead4c893d48680962e
Aaron Bieber <aaron@bolddaemon.com>
2020-08-05 11:49:50 -0600

    shrugumoji

--- common/dot_screenlayout/bg.sh
+++ common/dot_screenlayout/bg.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#feh --bg-fill ~/.background.png
+feh --bg-fill ~/.background.png
 #pkill xbattbar
 #xbattbar -I "#99aab9" -O "#798a99" -o "#99aab9" -i "#798a99" &
 #xbattbar -i green -o "olive drab" -I blue -O red &
 
 pkill lemonbar-xft
-while true; do ~/bin/bar; sleep 5; done | lemonbar-xft -d -f "Fantasque Sans Mono:pixelsize=14" -B "#FFFFEA"  -F "#000000" | sh &
+while true; do ~/bin/bar; sleep 5; done | lemonbar-xft -d -f "Go Mono:pixelsize=12" -B "#FFFFEA"  -F "#000000" | sh &
